73c0c0ba50775208007027b74d7ed2cacccf4883
morrison-turnansky <mturnans@redhat.com>
2026-06-01 14:24:32 +0000

    fix reverted ci

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -906,7 +906,11 @@ def trace_frame(
 ) -> DynamoTracerOutput:
     from torch.fx.experimental.validator import bisect, translation_validation_enabled
 
-    if torch.cuda.is_available() and torch.cuda.is_current_stream_capturing():
+    if (
+        hasattr(torch._C, "_cuda_isCurrentStreamCapturing")
+        and not isinstance(torch._C._cuda_isCurrentStreamCapturing, type)
+        and torch.cuda.is_current_stream_capturing()
+    ):
         raise exc.TorchRuntimeError(
             "torch.compile cannot JIT compile during CUDA graph capture. "
             "Execute warmup iterations outside of CUDA graph capture to trigger "
